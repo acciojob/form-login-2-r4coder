0ec5fc09c9bb2dba5e6d7ef31fdaf669cfa8c836
r4coder <ritheeshk2003@gmail.com>
2026-08-11 12:45:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ const form = document.getElementById("form");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        alert(
+            "First Name " + firstName.value + "\n" +
+            "Last Name " + lastName.value + "\n" +
+            "Phone Number " + phone.value + "\n" +
+            "Email ID " + email.value
+        );
+    });
